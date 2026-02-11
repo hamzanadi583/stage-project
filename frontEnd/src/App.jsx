854f4ea51b55eeb6846127177f0fae49ca5a00b3
hamzanadi583 <hamzanadi583@gmail.com>
2026-02-11 12:02:10 +0100

    Initial commit of full project (React + Laravel)

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./index.css";
+import Header from "./components/header";
+
 function App() {
   return (
-    <div className="bg-gray-300 h-screen">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ frontEnd/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow-sm py-2">
+       
+    </nav>
+  );
+};
+
+export default Header;
